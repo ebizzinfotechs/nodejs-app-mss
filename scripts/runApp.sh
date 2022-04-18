b8f9df18eaeab28b76518c337f1e2a6049f3b128
ebizzinfotechs <81732286+ebizzinfotechs@users.noreply.github.com>
2022-04-18 17:43:28 +0530

    Update runApp.sh

--- scripts/runApp.sh
+++ scripts/runApp.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env sh
-echo 'The following "npm" command runs your Node.js application'
-set -x
 npm start &
-sleep 1
-echo $! > .pidfile
-set +x
-echo 'Now you can'
-echo 'Visit http://ServerIp:9981 to see your Node.js application'
+
